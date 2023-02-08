c06e73cb0d970eeb346f694c2ee59f8cef4421ef
loliveiraof <120342933+loliveiraof@users.noreply.github.com>
2023-02-08 00:53:21 -0300

    commit 12
    site 99% pronto

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$("#navbar a, .btn, .btnback, #btnbackexp").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
